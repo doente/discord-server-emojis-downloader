ccd18cfa6d4941d44285380089435fab22381551
gu <56209673+strunged@users.noreply.github.com>
2022-08-05 02:12:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ async function main() {
         }
 
         response.data.pipe(fs.createWriteStream(`./emojis/${format}`));
-        console.log(`[${dones++}/${res.data.length}] Complete`)
+        console.log(`[${dones++}/${res.data.length}] Complete`);
     }
 };
 
